0cbd91a23974f98e384e50961dc7073f0bf49755
Alex <alexandros.tzoumas@outlook.com>
2016-09-04 23:24:16 +0300

    display temperatures for today in dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,16 @@
 const Measurement= require('../models/Measurement');
 
 exports.getDashboard = (req, res) => {
+
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     Measurement
         .find({
-            device:"0000000044FF92D2"
+            "createdAt" : { $gt: today }
         })
-        .limit(200)
         .sort({createdAt:-1})
+      //  .limit(200)
         .exec(function(err,data){
             console.log(data);
             res.render('dashboard', {
