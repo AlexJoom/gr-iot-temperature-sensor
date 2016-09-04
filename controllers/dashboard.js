8dc9ebe39de462827ea8592a31f04272e736b147
Alex <alexandros.tzoumas@outlook.com>
2016-09-04 22:57:52 +0300

    get temperatures

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,18 @@
-
+const Measurement= require('../models/Measurement');
 
 exports.getDashboard = (req, res) => {
-
-  var temperatureData =[];
-  res.render('dashboard', {
-    title: 'Dashboard',
-    menuItem: "dashboard",
-    measurements: temperatureData
-  });
+    Measurement
+        .find({
+            device:"0000000044FF92D2"
+        })
+        .limit(200)
+        .sort({createdAt:-1})
+        .exec(function(err,data){
+            console.log(data);
+            res.render('dashboard', {
+                title: 'Dashboard',
+                menuItem: "dashboard",
+                measurements: data
+            });
+    });
 };
